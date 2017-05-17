51255e3adb1f4eb5db7ef6197a2be2b8c6b98e19
lybglory <lybglory@outlook.com>
2017-05-17 22:32:07 +0800

    委托窗体传值

---
+++ 20151229/_06委托窗体传值/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06委托窗体传值
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //点击button1的时候，创建form2窗体，弹出form2窗体
+        private void button1_Click(object sender, EventArgs e)
+        {   //传入一个委托类型的参数，方法可以直接赋值给委托。
+            //那么就把委托所指向的方法赋值给这个构造函数参数。
+            //这样，form2窗体就拿到了SendLabel赋值方法。
+            Form2 fm2 = new Form2(SendLablel);
+            fm2.Show();
+        }
+
+        //传入string类型参数，把这个参数赋值给label1，这个传递过来的参数就是form2中的TexBox1.Text
+        //这个赋值方法，就是委托所指向的方法。也就说说，form2窗体拿到了这个赋值方法。
+        private void SendLablel(string str) {
+            label1.Text = str;      
+        }
+    }
+}
---
+++ 20151229/_06委托窗体传值/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06委托窗体传值
+{   //声明一个委托，指向的方法就是form1窗体中的SendLabel方法，签名必须保持一致
+    public delegate void DelSendString(string str);
+    public partial class Form2 : Form
+    {   //私有字段委托类型，用存储，方法
+        private DelSendString _delSend;
+        //构造函数,当创建form2对象的时候，传入一个委托，把给私有字段赋值
+        public Form2(DelSendString del)
+        {   //form1中如何可以访问form2中的委托呢？
+            //当form1创建form2对象的时候，传入一个委托类型参数进去，
+            //而方法又可以直接赋值给委托。当form1创建form2对象的时候，
+            //就会把委托所指向的方法赋值给了委托，
+            //form2中的构造函数就会把这个委托所指向的方法赋值给form2中的私有字段
+            //这样form2窗体就拿到了form1窗体的SendLabel方法
+            this._delSend = del;        
+            InitializeComponent();
+        }
+        //当点击form2窗体中的button1，就触发委托所指向的方法。
+        private void button1_Click(object sender, EventArgs e)
+        {   //委托所指向的方法是form1中SendLabel赋值方法。
+            _delSend(textBox1.Text);
+        }
+    }
+}
