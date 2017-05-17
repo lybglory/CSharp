3e674bfc8986b1cae5935005a7b98c8ea90fd3a8
lybglory <lybglory@outlook.com>
2017-05-17 22:25:25 +0800

    求出100~999之间的水仙花数

---
+++ 20151001/求水仙花/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 求水仙花
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 100; i < 999; i++)
+            {
+                int hundred = i / 100;
+                int tens = i % 100 / 10;
+                int units = i % 10;
+                
+                while (hundred*hundred*hundred+tens*tens*tens+units*units*units==i) {
+                    Console.WriteLine("水仙花数={0}", i);
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
