d09e5c70794b69b3308a31c08379f710a31a4320
lybglory <lybglory@outlook.com>
2017-05-17 22:23:09 +0800

    求出100以内所有质数

---
+++ 20151001/求100以内所有质数/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 求100以内所有质数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i++)
+            {
+                bool b = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        b = false;
+                        break;//不是质数
+                    }
+                }
+                if (b) {
+                    Console.WriteLine("质数：{0}", i);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
